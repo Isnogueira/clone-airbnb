9d3d25c47fd4f4d1e33ab732a77854d7b6bb3a4b
IngridNogueira <isnogueira601@gmail.com>
2025-09-22 17:38:59 -0300

    Torna os dados das acomodações dinamicos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { fetchData } from "@/utils/api";
 import Acomodacao from "@/widgets/acomodacoes";
 import Footer from "@/widgets/footer";
 import HorizontalTabNav from "@/widgets/horizontal-tab-nav";
 import Navbar from "@/widgets/navbar";
 import Seachbar from "@/widgets/seachbar";
 
-export default function Home() {
+export default async function Home() {
+  const dados =  await fetchData()
   return (
     <>
     <header className="container mx-auto">
@@ -13,8 +15,8 @@ export default function Home() {
     </header>          
     <hr className="my-6 opacity-10" />
     <main className="container mx-auto">
-      <HorizontalTabNav />
-      <Acomodacao />
+      <HorizontalTabNav icons={dados.icons} />
+      <Acomodacao accommodations={dados.accommodation}/>
     </main>
     <footer className="bg-gray-200">
         <Footer />
